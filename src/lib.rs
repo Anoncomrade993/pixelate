79cce0eb6ffae548baa0f795fa50529e54f3cfb8
Anoncomrade993 <ifeanyiotah001@gmail.com>
2024-05-24 22:36:49 +0100

    update lib.rs

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,6 @@
 ///perform lsb replacement 
 pub mod lsb{
-  pub fn encode(pixels:&mut Vec<u8>, data: &str, channel:u8) -> Result<Vec<u8>,&'static str>{
+  pub fn encode(pixels:&mut Vec<u8>, data: &str, channel:usize) -> Result<Vec<u8>,&'static str>{
       let mut tracker = 0;
       let data_len = data.len();
   
@@ -8,13 +8,13 @@ pub mod lsb{
          return Err("Inputs can't be null or empty")
       }
   
-      if Self::is_large(pixels.len(), data_len) {
+      if is_large(pixels.len(), data_len) {
         return Err("Data too big for pixels capacity")
       }
   
       for q in (0..pixels.len()).step_by(4){
           if tracker < data_len {
-              if let Some(pixel) = pixels.get_mut(q + *channel as usize) {
+              if let Some(pixel) = pixels.get_mut(q + *channel) {
                   *pixel &= 0xFE;
                   *pixel |= (data.chars().nth(tracker).unwrap() as u8 - b'0') & 1;
                   tracker += 1;
@@ -32,7 +32,7 @@ pub mod lsb{
       bin_len > (arr_len * 8) / 4
   }
   ///reverse the lsb replacement 
-  pub fn decode(pixels: Vec<u8>, channel:u8) -> Result<String, &'static str> {
+  pub fn decode(pixels: Vec<u8>, channel:usize) -> Result<String, &'static str> {
       let mut binary_string = String::new();
   
       if pixels.is_empty() {
@@ -40,7 +40,7 @@ pub mod lsb{
       }
   
       for q in (0..pixels.len()).step_by(4) {
-          let lsb = pixels[q + *channel as usize] & 1;
+          let lsb = pixels[q + *channel] & 1;
           binary_string.push_str(&lsb.to_string());
       }
   
